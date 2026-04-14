c0eb9260ef517ecdc92a90cad4c4fca5a78452be
Paweł Wypych <92308173+pawel00wypych@users.noreply.github.com>
2026-04-14 09:55:57 +0200

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.py
@@ -0,0 +1,54 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = [0]
+        self.k = k
+        for n in nums:
+            self.add(n)
+
+    def add(self, val: int) -> int:
+        if len(self.heap) < self.k + 1:
+            # maintain size of the heap = k (+ 1 is dummy node)
+            self.heapPush(val)
+        else:
+            if val > self.heap[1]:
+                # we add val only if its greater than min heap val
+                self.heapPop()
+                self.heapPush(val)
+
+        return self.heap[1]
+
+    def heapPush(self, val):
+        self.heap.append(val)
+        i = len(self.heap) - 1
+        while i > 1 and self.heap[i] < self.heap[i // 2]:
+            tmp = self.heap[i]
+            self.heap[i] = self.heap[i // 2]
+            self.heap[i // 2] = tmp
+            i = i // 2
+
+    def heapPop(self):
+        if len(self.heap) == 1:
+            return None
+        if len(self.heap) == 2:
+            return self.heap.pop()
+        res = self.heap[1]
+        self.heap[1] = self.heap.pop()
+        i = 1
+        while i * 2 < len(self.heap):
+            if (i * 2 + 1) < len(self.heap) and  self.heap[i * 2 + 1] < self.heap[i * 2] and self.heap[i * 2 + 1] < self.heap[i]:
+                # swap with right child
+                tmp = self.heap[i * 2 + 1]
+                self.heap[i * 2 + 1] = self.heap[i]
+                self.heap[i] = tmp
+                i = i * 2 + 1
+            elif self.heap[i * 2] < self.heap[i]:
+                # swap with left child
+                tmp = self.heap[i * 2]
+                self.heap[i * 2] = self.heap[i]
+                self.heap[i] = tmp
+                i = i * 2
+            else:
+                # node is in the right place
+                break
+        return res
