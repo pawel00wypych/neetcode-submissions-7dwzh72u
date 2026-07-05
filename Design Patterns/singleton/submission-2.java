46b7b179e4848dec4577c598363b037ea83ea2fb
Paweł Wypych <92308173+pawel00wypych@users.noreply.github.com>
2026-07-05 22:20:36 +0200

    Add: singleton - submission-2

---
+++ Design Patterns/singleton/submission-2.java
@@ -0,0 +1,30 @@
+static class Singleton {
+    private static volatile Singleton instance = null;
+
+    private String value = null;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized(Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+}
